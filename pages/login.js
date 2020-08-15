2c1b988f41138f65158935c0abe4ce7a14d0ede5
Jay Pan <lb0104672@gmail.com>
2020-08-15 18:20:47 +0800

    Add login page, which redirect to WordPress cms url

---
+++ pages/login.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Login() {
+  return (
+    <div>
+      Login
+    </div>
+  )
+}
+
+Login.getInitialProps = ({ res }) => {
+
+  if (res) {
+    res.writeHead(301, {
+      Location: process.env.CMS_URL
+    });
+    res.end();
+  }
+
+  return {};
+};
