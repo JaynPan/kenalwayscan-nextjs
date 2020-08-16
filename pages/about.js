bd75c5d71134362a88c6ebd141c4b972e65bfa04
Jay Pan <lb0104672@gmail.com>
2020-08-16 18:02:33 +0800

    Add about page

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,26 @@
+import Head from 'next/head';
+import styled from 'styled-components';
+
+import Layout from '@/components/Layout';
+import { TITLE } from '@/config/constants';
+
+const AboutWrapper = styled.main`
+  min-height: 100vh;
+  background-color: #fff;
+`;
+
 export default function about() {
   return (
-    <div>
-      about
-    </div>
+    <>
+      <Head>
+        <title>{`About | ${TITLE}`}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Layout>
+        <AboutWrapper>
+          about
+        </AboutWrapper>
+      </Layout>
+    </>
   );
 }
