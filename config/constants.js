67c490f18ac3599b452589be39e2ccccee94c28e
Jay Pan <lb0104672@gmail.com>
2020-08-16 15:36:02 +0800

    Add home page

--- config/constants.js
+++ config/constants.js
@@ -1,2 +1,3 @@
 // eslint-disable-next-line import/prefer-default-export
 export const API_URL = process.env.WORDPRESS_API_URL;
+export const { TITLE } = process.env;
--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,66 @@
 import Head from 'next/head';
-import Link from 'next/link';
+import styled from 'styled-components';
 
-import styles from '@/styles/Home.module.css';
-import { getAllPostsForHome } from '@/lib/api';
+import fetchHomeData from '@/lib/home/api';
+import { TITLE } from '@/config/constants';
 
-export default function Home({ allPosts }) {
+const HomeWrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+
+  /* CSS Smooth Scroll */
+  overflow-y: scroll;
+  scroll-behavior: smooth;
+  scroll-snap-type: y mandatory;
+  color: white;
+`;
+
+const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  width: 100%;
+  height: 100vh;
+
+  /* Scroll Snap */
+  scroll-snap-align: center;
+  scroll-snap-stop: always;
+`;
+
+export default function Home({ data }) {
+  const sliders = data?.acf?.slider || [];
+
   return (
-    <div className={styles.container}>
+    <HomeWrapper>
       <Head>
-        <title>Home | Next Mindset</title>
+        <title>{`Home | ${TITLE}`}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        {allPosts.map(({
-          id, title, excerpt, slug,
-        }) => (
-          <div key={id}>
-            <h3>{title.rendered}</h3>
-            <div dangerouslySetInnerHTML={{ __html: excerpt.rendered }} />
-            <Link as={`/projects/${slug}`} href="/projects/[slug]">
-              <a href="/">Read More</a>
-            </Link>
-          </div>
-        ))}
+        {sliders.length > 0
+          && sliders.map(({ image }) => {
+            const imageUrl = image.sizes.xxl;
+
+            return (
+              <Section
+                style={{
+                  background: `url('${imageUrl}') no-repeat center center/cover`,
+                }}
+              />
+            );
+          })}
       </main>
-    </div>
+    </HomeWrapper>
   );
 }
 
-export async function getStaticProps({ preview = false }) {
-  const allPosts = await getAllPostsForHome(preview);
+export async function getStaticProps() {
+  const data = await fetchHomeData();
+
   return {
-    props: { allPosts, preview },
+    props: { data },
     revalidate: 10,
   };
 }
