2ab072d67efc2a32fe81c5bb17ba68934711f8d2
Jay Pan <lb0104672@gmail.com>
2020-10-06 21:13:24 +0800

    Style: projects page image placholder container while loading

--- components/ProjectItem.js
+++ components/ProjectItem.js
@@ -13,6 +13,23 @@ const ArrowRight = styled.img`
   margin-right: 6px;
 `;
 
+const ImgWrapper = styled.div`
+  width: 100%;
+  height: 0;
+  padding-top: 66.66%;
+  position: relative;
+  background-color: #eee;
+  overflow: hidden;
+`;
+
+const Img = styled.img`
+  filter: ${(props) => (props.loading ? 'blur(5px)' : 'none')};
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+`;
+
 export default function ProjectItem({
  slug, title, date, image,
 }) {
@@ -20,15 +37,17 @@ export default function ProjectItem({
     <Link as={`/projects/${slug}`} href={`/projects/${slug}`}>
       <a>
         <div>
-          <ProgressiveImage src={image?.original} placeholder={image?.placeholder} delay={50}>
-            {(src, loading) => (
-              <img
-                style={{ filter: loading ? 'blur(5px)' : 'none' }}
-                src={src}
-                alt={title}
-              />
-          )}
-          </ProgressiveImage>
+          <ImgWrapper>
+            <ProgressiveImage src={image?.original} placeholder={image?.placeholder}>
+              {(src, loading) => (
+                <Img
+                  src={src}
+                  alt={title}
+                  loading={loading}
+                />
+              )}
+            </ProgressiveImage>
+          </ImgWrapper>
           <Info>
             <p style={{ flexGrow: 1 }}>{title}</p>
             <ArrowRight src="/right-arrow.svg" alt="" />
