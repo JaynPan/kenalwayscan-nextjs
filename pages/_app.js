041d3b7d1bc3ab36e260679aa1059e4348a3857f
Jay Pan <lb0104672@gmail.com>
2021-05-20 01:10:30 +0800

    remove lightbox css import

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,6 @@ import Router from 'next/router';
 import '@/styles/globals.css';
 import '@/styles/normalize.css';
 import 'react-image-gallery/styles/css/image-gallery.css';
-import 'react-image-lightbox/style.css';
 
 const handlers = {};
 
